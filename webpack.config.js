6c6477ebcf0e92895b058488f36d7c166261f519
Ashok Dey <ashokdey100@gmail.com>
2017-11-21 14:53:25 +0530

    --update : added saas loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: 'style-loader' }, // creates style nodes from JS strings
+          { loader: 'css-loader' }, // translates CSS into CommonJS
+          { loader: 'sass-loader' }, // compiles Sass to CSS
+        ],
+      },
     ],
   },
   // PLUGINS BELOW REDUCE BUNDLE SIZE FOR PRODUCTION
